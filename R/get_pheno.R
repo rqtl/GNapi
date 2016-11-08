00646f1bcef72797058ca549865b00c4a6f1a7f4
Karl Broman <kbroman@gmail.com>
2016-11-08 10:07:29 -0600

    Add function get_pheno()

---
+++ R/get_pheno.R
@@ -0,0 +1,30 @@
+#' Get phenotype data
+#'
+#' Get phenotype data for a dataset
+#'
+#' @param dataset Dataset name or ID as a single character string.
+#' @param url URL for GeneNetwork API
+#'
+#' @return Data frame with strain ID, strain name, phenotype value, and standard error.
+#'
+#' @importFrom httr GET content stop_for_status
+#' @export
+#'
+#' @examples
+#' get_pheno("10001")
+get_pheno <-
+    function(dataset, url="http://test-gn2.genenetwork.org/api_pre1/")
+{
+    # dataset should be a single character string
+    stopifnot(!is.null(dataset), length(dataset) == 1,  is.character(dataset))
+
+    result <- httr::GET(paste0(url, "trait/", dataset, ".json"))
+    listresult <- httr::content(result)
+    httr::stop_for_status(result)
+
+    data.frame(id=grab_elements(listresult, 1, as.numeric(NA)),
+               strain=grab_elements(listresult, 2, as.character(NA)),
+               value=grab_elements(listresult, 3, as.numeric(NA)),
+               SE=grab_elements(listresult, 4, as.numeric(NA)),
+               stringsAsFactors=FALSE)
+}
---
+++ tests/testthat/test-get_pheno.R
@@ -0,0 +1,14 @@
+context("get_pheno")
+
+test_that("get_pheno works", {
+
+    df <- get_pheno("10001")
+    expect_true(is.data.frame(df))
+    expect_true(ncol(df) == 4)
+    expect_equal(colnames(df), c("id", "strain", "value", "SE"))
+    expect_true(is.numeric(df[,1]))
+    expect_true(is.character(df[,2]))
+    expect_true(is.numeric(df[,3]))
+    expect_true(is.numeric(df[,4]))
+
+})
