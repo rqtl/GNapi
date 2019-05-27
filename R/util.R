bf37bcb434280c402719804f0407ac74a425f623
Karl Broman <kbroman@gmail.com>
2019-05-26 20:34:23 -0500

    Remove the rest of the old stuff

--- R/util.R
+++
@@ -1,7 +0,0 @@
-# grab some element from each of a list of lists
-# if NULL, convert to NA using a particular type
-grab_elements <-
-    function(v, index, null_value=as.character(NA))
-{
-    sapply(v, function(a) ifelse(length(a) < index || is.null(a[[index]]), null_value, a[[index]]))
-}
