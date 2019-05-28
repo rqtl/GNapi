307aefd54aeeb15307a6b15dcc0e9ef17e77cf7d
Karl Broman <kbroman@gmail.com>
2019-05-28 13:01:16 -0500

    get_pheno(): samples as rows, ideally with IDs in row names

--- R/get_data.R
+++ R/get_data.R
@@ -6,15 +6,15 @@
 #' @param trait Optional trait name
 #' @param url The URL for the GeneNetwork API
 #'
-#' @return A data frame, either with traits as rows and samples as columns,
-#' or with each sample as a row and columns being detailed information for a single trait
+#' @return A data frame, with samples as rows and either traits as columns or
+#' with the columns being detailed information for a single trait
 #'
 #' @seealso [get_geno()]
 #'
 #' @export
 #'
 #' @examples
-#' ph1 <- get_pheno("B30_K_1206_R")
+#' \donttest{ph1 <- get_pheno("B30_K_1206_R")}
 #' ph2 <- get_pheno("BXD", "10002")
 #' ph3 <- get_pheno("HC_M2_0606_P", "1436869_at")
 get_pheno <-
@@ -23,12 +23,32 @@ get_pheno <-
     stopifnot(length(dataset)==1)
 
     if(is.null(trait)) {
-        result <- as.data.frame( query_gn(paste0("sample_data/", dataset), url),
-                                 stringsAsFactors=FALSE )
+        result <- query_gn(paste0("sample_data/", dataset), url, output="text")
+        result <- read.table(text=result, sep=",", header=TRUE,
+                             na.strings="x", stringsAsFactors=FALSE )
+
+        # trait names as row names
+        if(is.character(result[,1]) && length(unique(result[,1])) == nrow(result)) {
+            rownames(result) <- result[,1]
+            result <- result[,-1]
+        }
+
+        # samples as rows, traits as columns
+        result <- t(result)
+
     } else {
         stopifnot(length(trait)==1)
         result <- query_gn(paste0("sample_data/", dataset, "/", trait), url)
         result <- list2df(result)
+
+        # sample IDs as row names?
+        if("sample_name" %in% colnames(result)) {
+            if(length(unique(result$sample_name)) == nrow(result)) {
+                rownames(result) <- result$sample_name
+            } else {
+                warning("sample_name values are not distinct")
+            }
+        }
     }
 
     result
